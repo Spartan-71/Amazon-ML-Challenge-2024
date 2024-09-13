e1ecb14828a4ae9cb02252cdbab0317484a3abf0
Anish Dabhane <anishdabhane71@gmail.com>
2024-09-13 23:42:20 +0530

    modularized

--- Hackathon/src/ocr_converter.py
+++ Hackathon/src/ocr_converter.py
@@ -1,45 +1,66 @@
-from PIL import Image
+import cv2
+import re
 import pytesseract
 import pandas as pd
-import re
+import units as ut
+from PIL import Image
 from tqdm import tqdm
 
-def preprocess(text :str) -> str:
+def loader(dataset_path,no_of_sample):
+    df = pd.read_csv(dataset_path)
+    df = df.head(no_of_sample)
+    values = pd.Series(dtype='string')
+    pre_values= pd.Series(dtype='string')
+    contains = pd.Series(dtype='string')
+
+
+def preprocessing(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #add more preprocessing if the quality increases
+    return gray
+
+def ocr(preprocessed_img) -> str:
+    tes_text = pytesseract.image_to_string(preprocessed_img, lang='eng')
+    return tes_text
+
+
+def postprocessing(text: str) -> list:
     lower_case= text.lower()
+    lower_case.replace('\n', '')
     symbols = r'[ !@#%^&*()$_\-\+\{\}\[\]\'\|:;"<>,./~?`=\"©™°®¢»«¥“”§—‘’é€]'
     alphabets = r'[jsxyz]'
+    pattern = r'(\d+..)'
     cleaned_symbol = re.sub(symbols, '', lower_case)
-    cleaned_text = re.sub(alphabets, '', cleaned_symbol)
-    print(cleaned_text)
+    # cleaned_text = re.sub(alphabets, '', cleaned_symbol)
+    cleaned_text = re.findall(pattern, cleaned_symbol)
+    # print(cleaned_text)
     return cleaned_text
 
+if __name__== "__main__" :
 
-df = pd.read_csv('../dataset/train.csv')
-df = df.head(100)
+    # loader('../dataset/train.csv',100)
+    df = pd.read_csv('../dataset/train.csv')
+    df = df.head(10)
+    values = pd.Series(dtype='string')
+    list= pd.Series(dtype='object')
+    contains = pd.Series(dtype='string')
 
-print("Loaded Data")
+    print("Data Loaded :)")
 
-values = pd.Series(dtype='string')
-pre_values= pd.Series(dtype='string')
-contains = pd.Series(dtype='string')
+    for i, link in tqdm(enumerate(df['image_link']), total=len(df)):
 
-for i, link in tqdm(enumerate(df['image_link']), total=len(df)):
+        link = link.split('/')[-1]
+        file_name = re.findall(r'.*\.jpg', link)[0]
 
-    link = link.split('/')[-1]
-    file_name = re.findall(r'.*\.jpg', link)[0]
+        img =preprocessing(f'../images/{file_name}')
+        tes_text = ocr(img)
+        output = postprocessing(tes_text)
+        list.at[i]=output
 
-    img = Image.open(f'../images/{file_name}')
+    # df['tessaract_values'] = values
+    df['List']=list
 
-    # Tessaract
-    tes_text = pytesseract.image_to_string(img, lang='eng')
-    tes_text = tes_text.replace(' ', '').replace('\n', '')
-    pre_tes_text = preprocess(tes_text)
-    # values.at[i] = tes_text
-    pre_values.at[i]=pre_tes_text
-
-# df['tessaract_values'] = values
-df['preprocessed']=pre_values
-
-csv_file_path = 'output.csv'
-df.to_csv(csv_file_path, index=False)  
+    csv_file_path = 'output.csv'
+    df.to_csv(csv_file_path, index=False)  
 
