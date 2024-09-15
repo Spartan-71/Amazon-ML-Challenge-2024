599308eee243ca3ae7c69d5ffa1773f5bcfaf40b
Anish Dabhane <anishdabhane71@gmail.com>
2024-09-15 21:37:49 +0530

    script updated : generate_output_local.py

--- student_resource/generate_output_local.py
+++ student_resource/generate_output_local.py
@@ -3,7 +3,7 @@ import pandas as pd
 import cv2
 import re
 from tqdm import tqdm
-import src.units as ut
+import src.single_units as ut
 
 
 def loader(dataset_path, no_of_sample=10):
@@ -23,14 +23,22 @@ def preprocessing(img_path):
 
 def postprocessing(text: str) -> list:
     # Clean and extract relevant numerical data from OCR text.
-    lower_case = text.lower().replace('\n', '')
+
+    #removing all the spaces /tabs ...
+    no_spaces=re.sub(r"\s+", "", text)
+    #lowercasing
+    lower_case = no_spaces.lower()
     symbols = r'[ !@#%^&*()$_\-\+\{\}\[\]\'\|:;"<>,/~?`=\"©™°®¢»«¥“”§—‘’é€]'
-    alphabets = r'[jsxyz]'
     # pattern = r'(\d+..)'
-    pattern = r'(\d+(\.\d+)?\w{2})'
-    cleaned_symbol = re.sub(symbols, ' ', lower_case)
-    cleaned_text = re.findall(pattern, cleaned_symbol)
-    cleaned_text = [item for tup in cleaned_text for item in tup]
+    # pattern = r'(\d+(\.\d+)?\w{2})'
+    # pattern = r'(\d+(\.\d{1,})?\w{2})'
+    pattern = r'(\d+(\.\d+)?\w)'
+    cleaned_symbol = re.sub(symbols, ' ', lower_case) 
+    cleaned_text = re.findall(pattern, cleaned_symbol) # returns a list of tuples
+    cleaned_text = [item for tup in cleaned_text for item in tup] # returns a list of strings
+    print('----------------------------------------------------------------------------------------')
+    print(f"clr_sym: {cleaned_symbol}")
+    print(f"clr_text: {cleaned_text}")
     return cleaned_text
 
 def match_units(input_list, units_dict, entity_name):
@@ -43,28 +51,35 @@ def match_units(input_list, units_dict, entity_name):
     results = []
     for item in input_list:
         if len(item) >= 2:
-            last_two_chars = item[-2:]
-            result = abb_dict.get(last_two_chars)
+            last_one_char = item[-1:]
+            result = abb_dict.get(last_one_char)
             if result:
-                results.append(f"{item[:-2]} {result}")
+                results.append(f"{item[:-1]} {result}")
+            # else:
+            #     last_one_char = item[-1:]
+            #     result=abb_dict.get(last_one_char)
+            #     results.append(f"{item[:-2]} {result}")
 
+    print(f"matched units: {results}")
     return results
 
 def get_max(units_list):
     if len(units_list) == 0:
-        return ""
+        return ""    # print(f"fo: {final_output}")
 
-    max_val = -10000
+
+    max_val = -10000000
     max_val_unit = units_list[0].split(" ")[1]
     for u in units_list:
         parts = u.split(' ')
+        print(f"parts: {parts}")
         num = parts[0]
         if '.' in num:
             num = float(num)
         else:
             num = int(num)
 
-        unit = parts[1]
+        unit = ' '.join(parts[1:])
         if num > max_val:
             max_val = num
             max_val_unit = unit
@@ -76,10 +91,12 @@ def predictor(image_link, category_id, entity_name, text):
     Call your model/approach here
     '''
     #TODO
+    print(f"cat: {entity_name}")
     output = postprocessing(text)
     matched_units = match_units(output, ut.units_dict, entity_name)
-
     final_output = get_max(matched_units)
+    # print(f"out: {output}")
+    print(f"fo: {final_output}")
     return final_output
 
 if __name__ == "__main__":
@@ -98,7 +115,7 @@ if __name__ == "__main__":
         data_text = data.at[i, 'e']
         if pd.isna(data_text):
             continue
-        test.at[i, 'prediction'] = predictor(row['image_link'], row['group_id'], row['entity_name'], data_text)
+        test.at[i,'prediction'] = predictor(row['image_link'], row['group_id'], row['entity_name'], data_text)
 
     # Set output file
     output_filename = os.path.join(DATASET_FOLDER, 'test_out.csv')
---
+++ student_resource/src/single_units.py
@@ -0,0 +1,94 @@
+units_dict = {
+    'width':{
+        'c':'centimeter',
+        'cm': 'centimeter',
+        'm': 'meter',
+        'mm': 'millimeter',
+        'i':'inch',
+        'in': 'inch',
+        'f':'foot',
+        'ft': 'foot',
+        'y':'yard',
+        'yd': 'yard'
+    },
+
+    'depth':{
+        'c':'centimeter',
+        'cm': 'centimeter',
+        'm': 'meter',
+        'mm': 'millimeter',
+        'i':'inch',
+        'in': 'inch',
+        'f':'foot',
+        'ft': 'foot',
+        'y':'yard',
+        'yd': 'yard'
+    },
+
+   'height':{
+        'c':'centimeter',
+        'cm': 'centimeter',
+        'm': 'meter',
+        'mm': 'millimeter',
+        'i':'inch',
+        'in': 'inch',
+        'f':'foot',
+        'ft': 'foot',
+        'y':'yard',
+        'yd': 'yard'
+    },
+
+    'item_weight':{
+        'g': 'gram',
+        'k':'kilogram',
+        'kg': 'kilogram',
+        'µ':'microgram',
+        'u':'microgram',
+        'µg': 'microgram',
+        'm':'milligram',
+        'mg': 'milligram',
+        'o':'ounce',
+        'oz': 'ounce',
+        'l':'pound',
+        'lb': 'pound',
+        't': 'ton'
+    },
+
+    'voltage':{'k':'kilovolt','kv': 'kilovolt','m':'millivolt','mv': 'millivolt','v': 'volt'},
+    'wattage':{'k':'kilowatt','kw': 'kilowatt','w': 'watt'},
+
+    'item_volume':{
+        'l': 'liter',
+        'm':'milliliter',
+        'ml': 'milliliter',
+        'c':'centiliter',
+        'cl': 'centiliter',
+        'd':'deciliter',
+        'dl': 'deciliter',
+        'u':'microliter',
+        'µ':'microliter',
+        'µl': 'microliter',
+        'f':'cubic foot',
+        'ft':'cubic foot',
+        'ft³': 'cubic foot',
+        'i':'cubic inch',
+        'in':'cubic inch',
+        'in³': 'cubic inch',
+        'cu':'cup',
+        'cup': 'cup',
+        'fl':'fluid ounce',
+        'flo':'fluid ounce',
+        'floz': 'fluid ounce',
+        'g':'gallon',
+        'ga':'gallon',
+        'gal': 'gallon',
+        'im':'imperial gallon',
+        'imp':'imperial gallon',
+        'impgal': 'imperial gallon',
+        'p':'pint',
+        'pt': 'pint',
+        'q':'quart',
+        'qt': 'quart'
+    }
+    
+}
