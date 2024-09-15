b6a15e47e7415c855751114a579cf44ac873c4f7
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2024-09-15 10:49:11 +0530

    Cleaned data also total

--- student_resource/src/data_cleaner.py
+++ student_resource/src/data_cleaner.py
@@ -1,13 +1,11 @@
 import pandas as pd
-import easyocr
-
-reader = easyocr.Reader(['en'])
 
 df = pd.read_csv('./checkpoints/full_test_output.csv', index_col=0)
-test_df = pd.read_csv('./dataset/test.csv', index_col=0)
+test_df = pd.read_csv('./dataset/test.csv')
 
 total_in_data = len(df.iloc[:])
-total_in_test = len(test_df.iloc[:])
+total_in_test = test_df.iloc[-1]['index'] + 1
+print(total_in_test)
 
 df = df.reindex(range(total_in_test))
 
@@ -15,11 +13,11 @@ missing = []
 for i in range(total_in_test):
     val = df.at[i, 'e']
     if(pd.isna(val)):
-        print(df.iloc[i])
+        # print(df.iloc[i])
         missing.append(i)
 
 
 print(len(missing))
 
-df.to_csv('./cleaned_data.csv')
+df.to_csv('./cleaned_data2.csv')
 print("written")
